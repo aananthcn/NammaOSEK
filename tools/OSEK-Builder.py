b6488364ff49dcba9cc605d2a48fe825c004b878
Aananth C N <c.n.aananth@gmail.com>
2021-07-10 12:42:00 +0530

    Completed reading OS & CPU OIL inputs

--- tools/OSEK-Builder.py
+++ tools/OSEK-Builder.py
@@ -4,12 +4,24 @@
 import sys
 import utils
 
+def read_os_attribute(sheet, sheetname, attr_col, attr):
+    col, row = utils.locate_text_in_column(attr, sheet, attr_col)
+    if row == -1:
+        print("Error: can't locate \"CPU\" in sheet \"" + sheetname + "\"")
+        return
+    attr_val = sheet[chr(col+1)+str(row)].value
+    if attr_val == True:
+        attr_val = "TRUE"
+    if attr_val == False:
+        attr_val = "FALSE"
+    
+    return attr_val
 
 def parse_os_data(wb, sheetname):
-    cpu = os_name = status = startuphook = errorhook = shutdownhook = ""
-    pretaskhook = posttaskhook = usegetserviceid = useparameteraccess = ""
-    useresscheduler = ""
+    cpu = os_name = status = strthk = errhk = sdnhk = ""
+    prtskhk = potskhk = ugtsvid = uparacc = usrsch = ""
 
+    # validate "OS" sheet and find column to read attributes
     col, row = utils.locate_heading_column("Attribute Name", wb, sheetname)
     if col == -1:
         print("Error: can't locate \"Attribute Name\" in sheet \"" + sheetname + "\"")
@@ -17,13 +29,22 @@ def parse_os_data(wb, sheetname):
     attr_col = col
     os_sheet = wb[sheetname]
 
-    col, row = utils.locate_text_in_column("CPU", os_sheet, attr_col)
-    if row == -1:
-        print("Error: can't locate \"CPU\" in sheet \"" + sheetname + "\"")
-        return
-    cpu = os_sheet[chr(col+1)+str(row)].value
-    print("cpu = " + cpu)
+    # read CPU
+    cpu     = read_os_attribute(os_sheet, sheetname, attr_col, "CPU")
+    os_name = read_os_attribute(os_sheet, sheetname, attr_col, "OS NAME")
+    status  = read_os_attribute(os_sheet, sheetname, attr_col, "STATUS")
+    strthk  = read_os_attribute(os_sheet, sheetname, attr_col, "STARTUPHOOK")
+    errhk   = read_os_attribute(os_sheet, sheetname, attr_col, "ERRORHOOK")
+    sdnhk   = read_os_attribute(os_sheet, sheetname, attr_col, "SHUTDOWNHOOK")
+    prtskhk = read_os_attribute(os_sheet, sheetname, attr_col, "PRETASKHOOK")
+    potskhk = read_os_attribute(os_sheet, sheetname, attr_col, "POSTTASKHOOK")
+    ugtsvid = read_os_attribute(os_sheet, sheetname, attr_col, "USEGETSERVICEID")
+    uparacc = read_os_attribute(os_sheet, sheetname, attr_col, "USEPARAMETERACCESS")
+    usrsch  = read_os_attribute(os_sheet, sheetname, attr_col, "USERESSCHEDULER")
 
+    print("OS:" + cpu, os_name, status, strthk, errhk, sdnhk, prtskhk, potskhk, ugtsvid, uparacc, usrsch)
+
+    # read OS name
 
 def main(wb):
     for sheetname in wb.sheetnames:
