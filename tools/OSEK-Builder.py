3e8457ab4f5622db05ec56d0498b77c615ebec07
Aananth C N <c.n.aananth@gmail.com>
2021-07-10 13:23:54 +0530

    Added support for AppMode parsing

--- tools/OSEK-Builder.py
+++ tools/OSEK-Builder.py
@@ -4,6 +4,37 @@
 import sys
 import utils
 
+import colorama
+from colorama import Fore, Back, Style
+
+
+def validate_column(wb, sheetname, col_header):
+    col, row = utils.locate_heading_column(col_header, wb, sheetname)
+    if col == -1:
+        print("Error: can't locate \""+col_header+"\" in sheet \"" + sheetname + "\"")
+        return None
+
+    return col, row
+
+
+
+def parse_appmode_data(wb, sheetname):
+    attr_col, attr_row = validate_column(wb, sheetname, "APPMODE")
+    if attr_col == None:
+        return None
+
+    sheet = wb[sheetname]
+    AppModes = []
+    active_rows = len(sheet[chr(attr_col)])
+    for i in range(attr_row, active_rows):
+        row = i + attr_row
+        AppModes.append(sheet[chr(attr_col)+str(row)].value.strip())
+    
+    print(AppModes)
+    return AppModes
+
+
+
 def read_os_attribute(sheet, sheetname, attr_col, attr):
     col, row = utils.locate_text_in_column(attr, sheet, attr_col)
     if row == -1:
@@ -18,38 +49,35 @@ def read_os_attribute(sheet, sheetname, attr_col, attr):
     return attr_val
 
 def parse_os_data(wb, sheetname):
-    cpu = os_name = status = strthk = errhk = sdnhk = ""
-    prtskhk = potskhk = ugtsvid = uparacc = usrsch = ""
+    attr_col, attr_row = validate_column(wb, sheetname, "Attribute Name")
+    if attr_col == None:
+        return None
+    sheet = wb[sheetname]
+    OsData   = {}
 
-    # validate "OS" sheet and find column to read attributes
-    col, row = utils.locate_heading_column("Attribute Name", wb, sheetname)
-    if col == -1:
-        print("Error: can't locate \"Attribute Name\" in sheet \"" + sheetname + "\"")
-        return
-    attr_col = col
-    os_sheet = wb[sheetname]
+    # read os data
+    OsData["cpu"]      = read_os_attribute(sheet, sheetname, attr_col, "CPU")
+    OsData["os_name"]  = read_os_attribute(sheet, sheetname, attr_col, "OS NAME")
+    OsData["status"]   = read_os_attribute(sheet, sheetname, attr_col, "STATUS")
+    OsData["strthk"]   = read_os_attribute(sheet, sheetname, attr_col, "STARTUPHOOK")
+    OsData["errhk"]    = read_os_attribute(sheet, sheetname, attr_col, "ERRORHOOK")
+    OsData["shdnhk"]   = read_os_attribute(sheet, sheetname, attr_col, "SHUTDOWNHOOK")
+    OsData["pretskhk"] = read_os_attribute(sheet, sheetname, attr_col, "PRETASKHOOK")
+    OsData["postskhk"] = read_os_attribute(sheet, sheetname, attr_col, "POSTTASKHOOK")
+    OsData["ugtsrvid"] = read_os_attribute(sheet, sheetname, attr_col, "USEGETSERVICEID")
+    OsData["uparacc"]  = read_os_attribute(sheet, sheetname, attr_col, "USEPARAMETERACCESS")
+    OsData["uressch"]  = read_os_attribute(sheet, sheetname, attr_col, "USERESSCHEDULER")
 
-    # read CPU
-    cpu     = read_os_attribute(os_sheet, sheetname, attr_col, "CPU")
-    os_name = read_os_attribute(os_sheet, sheetname, attr_col, "OS NAME")
-    status  = read_os_attribute(os_sheet, sheetname, attr_col, "STATUS")
-    strthk  = read_os_attribute(os_sheet, sheetname, attr_col, "STARTUPHOOK")
-    errhk   = read_os_attribute(os_sheet, sheetname, attr_col, "ERRORHOOK")
-    sdnhk   = read_os_attribute(os_sheet, sheetname, attr_col, "SHUTDOWNHOOK")
-    prtskhk = read_os_attribute(os_sheet, sheetname, attr_col, "PRETASKHOOK")
-    potskhk = read_os_attribute(os_sheet, sheetname, attr_col, "POSTTASKHOOK")
-    ugtsvid = read_os_attribute(os_sheet, sheetname, attr_col, "USEGETSERVICEID")
-    uparacc = read_os_attribute(os_sheet, sheetname, attr_col, "USEPARAMETERACCESS")
-    usrsch  = read_os_attribute(os_sheet, sheetname, attr_col, "USERESSCHEDULER")
+    print(OsData)
+    return OsData
 
-    print("OS:" + cpu, os_name, status, strthk, errhk, sdnhk, prtskhk, potskhk, ugtsvid, uparacc, usrsch)
-
-    # read OS name
 
 def main(wb):
     for sheetname in wb.sheetnames:
         if "OS" in sheetname:
-            parse_os_data(wb, sheetname)
+            OsData = parse_os_data(wb, sheetname)
+        if "APPMODE" in sheetname:
+            AppModes = parse_appmode_data(wb, sheetname)
 
 
 def print_usage(prog):
@@ -65,10 +93,13 @@ if __name__ == '__main__':
     
     # check and import pre-requisites
     utils.import_or_install("openpyxl")
+    utils.import_or_install("colorama")
 
     excelfile = sys.argv[1]
     if "xlsx" != excelfile.split(".")[-1]:
         print("Error: Input file is not an excel file!")
     else:
+        print(Fore.BLUE)
         wb = utils.open_excel_file(excelfile)
+        print(Style.RESET_ALL)
         main(wb)
