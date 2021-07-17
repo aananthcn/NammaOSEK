e2fdb7e9f01a19616d60434148e9666a5e279281
Aananth C N <c.n.aananth@gmail.com>
2021-07-17 17:19:08 +0530

    Added support for ISR in OSEK Builder tool

---
+++ tools/scripts/ob_globals.py
@@ -0,0 +1,17 @@
+
+# OSEK Builder Global Variables (OB Globals)
+# ------------------------------------------
+# list of column titles in TASK tab of OSEX-Builder.xlsx
+TaskParams = ["Task Name", "Priority", "Schedule", "Activation", "Autostart",
+    "Resource", "Event", "Message"]
+
+# list of column titles in TASK tab of OSEX-Builder.xlsx
+CntrParams = ["Counter Name", "MINCYCLE", "MAXALLOWEDVALUE", "TICKSPERBASE"]
+
+# Column titles for Alarms
+AlarmParams = ["Alarm Name", "COUNTER", "Action-Type", "arg1", "arg2", "IsAutostart",
+	"ALARMTIME", "CYCLETIME", "APPMODE[]" ]
+
+# Column titles for ISRs
+ISR_Params = ["ISR Name", "CATEGORY", "RESOURCE", "MESSAGE"]
+
