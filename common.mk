8dccfa2c47b3ca1f702b90626bb8d5f89a298f5e
Aananth C N <c.n.aananth@gmail.com>
2021-07-18 12:44:13 +0530

    Undo common.mk deletion

---
+++ common.mk
@@ -0,0 +1,21 @@
+TARGET = osek-os
+
+OBJS	:= $(BRD_OBJS) $(LIBOBJS)
+
+
+.PHONY: all
+
+all: ${TARGET}
+
+${TARGET}: ${OBJS}
+	@echo Compiled for -march=${ARCH}
+	$(LD) $^ -o ${TARGET}.elf $(LDFLAGS) 
+	$(OBJCOPY) -O binary ${TARGET}.elf ${TARGET}.bin
+
+info:
+	@echo make can be run with var ARCH=${ARCH}
+	@echo By default ARCH=arm64
+
+clean:
+	rm -f ${OBJS}
+	rm -f ${TARGET}.bin ${TARGET}.elf
--- tools/scripts/sg_counter.py
+++ tools/scripts/sg_counter.py
@@ -1,8 +1,11 @@
 from common import *
 from ob_globals import *
 
+import colorama
+from colorama import Fore, Back, Style
 
-C_Counter_Type = "typedef struct {\n\
+
+C_Counter_Type = "\n\ntypedef struct {\n\
 \tchar* name;\n\
 \tu32 mincycle;\n\
 \tu32 maxallowedvalue;\n\
@@ -12,34 +15,57 @@ C_Counter_Type = "typedef struct {\n\
 
 def generate_code(path, Counters):
     print_info("Generating code for counters")
+    os_counter_index = -1
+    os_counter_duration = -1
 
     # create header file
     filename = path + "/" + "sg_counter.h"
-    f = open(filename, "w")
-    f.write("#ifndef ACN_OSEK_SG_COUNTER_H\n")
-    f.write("#define ACN_OSEK_SG_COUNTER_H\n")
-    f.write("#include <osek.h>\n")
-    f.write("\n\n#define OS_MAX_COUNTERS    ("+str(len(Counters))+")\n\n")
-    f.write(C_Counter_Type)
-    f.write("\n\n#endif\n")
-    f.close
+    hf = open(filename, "w")
+    hf.write("#ifndef ACN_OSEK_SG_COUNTER_H\n")
+    hf.write("#define ACN_OSEK_SG_COUNTER_H\n")
+    hf.write("#include <osek.h>\n")
+    hf.write(C_Counter_Type)
+    hf.write("OsCounterType OsCounters[];\n")
 
     # create source file
     filename = path + "/" + "sg_counter.c"
-    f = open(filename, "w")
-    f.write("#include \"sg_counter.h\"\n")
-    f.write("\n\nOsCounterType OsCounters[] =  {\n")
+    cf = open(filename, "w")
+    cf.write("#include \"sg_counter.h\"\n")
+    cf.write("\n\nOsCounterType OsCounters[] =  {\n")
     for i, cntr in enumerate(Counters):
-        f.write("\t{\n")
-        f.write("\t\t.name = \""+ cntr[CntrParams[0]] + "\",\n")
-        f.write("\t\t.mincycle = "+ str(cntr[CntrParams[1]]) + ",\n")
-        f.write("\t\t.maxallowedvalue = "+ str(cntr[CntrParams[2]]) + ",\n")
-        f.write("\t\t.ticksperbase = "+ str(cntr[CntrParams[3]]) + ",\n")
-        f.write("\t\t.tickduration = "+ str(cntr[CntrParams[4]]) + "\n")
-        f.write("\t}")
+        cf.write("\t{\n")
+        cf.write("\t\t.name = \""+ cntr[CntrParams[0]] + "\",\n")
+        cf.write("\t\t.mincycle = "+ str(cntr[CntrParams[1]]) + ",\n")
+        cf.write("\t\t.maxallowedvalue = "+ str(cntr[CntrParams[2]]) + ",\n")
+        cf.write("\t\t.ticksperbase = "+ str(cntr[CntrParams[3]]) + ",\n")
+        cf.write("\t\t.tickduration = "+ str(cntr[CntrParams[4]]) + "\n")
+        cf.write("\t}")
         if i+1 == len(Counters):
-            f.write("\n")
+            cf.write("\n")
         else:
-            f.write(",\n")
-    f.write("};")
-    f.close
+            cf.write(",\n")
+        
+        # Find out if this Counter is optimal for OS Tick scheduling
+        if cntr[CntrParams[4]] >= 1000000: #nano sec
+            if os_counter_duration == -1:
+                os_counter_duration = cntr[CntrParams[4]]
+                os_counter_index = i
+            elif cntr[CntrParams[4]] < os_counter_duration: 
+                os_counter_duration = cntr[CntrParams[4]]
+                os_counter_index = i
+
+    cf.write("};\n")
+    # close source file
+    cf.close()
+
+    # close header file
+    if os_counter_duration == -1 or os_counter_index == -1:
+        print(Fore.YELLOW+"\nWarning: None of the counter is (>= 1ms) is fit for OS tick timer!\n" +
+            "Please check the OSEK-Builder.xlsx and configure COUNTER correctly!\n"+Style.RESET_ALL)
+    else:
+	    hf.write("\n#define OS_TICK_DURATION_ns \t("+str(os_counter_duration)+")")
+	    hf.write("\n#define OS_TICK_COUNTER_IDX \t("+str(os_counter_index)+")")
+    hf.write("\n#define OS_MAX_COUNTERS    \t("+str(len(Counters))+")\n")
+    
+    hf.write("\n\n#endif\n")
+    hf.close()
