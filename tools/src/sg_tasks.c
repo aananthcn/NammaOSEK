954cbcdf674d43766fff2439522af7bfd0973b30
Aananth C N <c.n.aananth@gmail.com>
2021-07-28 22:17:59 +0530

    Initialized event masks, max values for event and appmodes
    TasksList structure, which is auto generated from OIL file now improved
    with the new sets of inits.

--- tools/src/sg_tasks.c
+++ tools/src/sg_tasks.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include "sg_tasks.h"
+#include "sg_events.h"
 
 
 /*   T A S K   D E F I N I T I O N   */
@@ -17,12 +18,21 @@ const AppModeType Task_C_AppModes[] = {
 
 const OsTaskType OsTaskList[3] = {
 	{
-		.app_modes = (const AppModeType **) &Task_A_AppModes
+		.app_modes = (const AppModeType **) &Task_A_AppModes,
+		.n_app_modes = TASK_A_APPMODE_MAX,
+		.evt_msk = NULL,
+		.n_evt = TASK_A_EVENT_MAX
 	},
 	{
-		.app_modes = NULL
+		.app_modes = NULL,
+		.n_app_modes = TASK_B_APPMODE_MAX,
+		.evt_msk = (const EventMaskType**) &Task_B_EventMasks,
+		.n_evt = TASK_B_EVENT_MAX
 	},
 	{
-		.app_modes = (const AppModeType **) &Task_C_AppModes
+		.app_modes = (const AppModeType **) &Task_C_AppModes,
+		.n_app_modes = TASK_C_APPMODE_MAX,
+		.evt_msk = (const EventMaskType**) &Task_C_EventMasks,
+		.n_evt = TASK_C_EVENT_MAX
 	}
 };
