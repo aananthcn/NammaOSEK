4a6bab7a4e7ff830224fc72ad795166dd43e8bc2
Aananth C N <c.n.aananth@gmail.com>
2021-07-29 11:22:33 +0530

    Added msg length to OS Task list.

--- tools/src/sg_tasks.c
+++ tools/src/sg_tasks.c
@@ -12,20 +12,23 @@ const OsTaskType OsTaskList[3] = {
 		.n_appmodes = TASK_A_APPMODE_MAX,
 		.evtmsks = NULL,
 		.n_evtmsks = TASK_A_EVENT_MAX,
-		.msglist = (MessageType**) &Task_A_Messages
+		.msglist = (MessageType**) &Task_A_Messages,
+		.n_msglist = TASK_A_MESSAGE_MAX
 	},
 	{
 		.appmodes = NULL,
 		.n_appmodes = TASK_B_APPMODE_MAX,
 		.evtmsks = (const EventMaskType**) &Task_B_EventMasks,
 		.n_evtmsks = TASK_B_EVENT_MAX,
-		.msglist = (MessageType**) &Task_B_Messages
+		.msglist = (MessageType**) &Task_B_Messages,
+		.n_msglist = TASK_B_MESSAGE_MAX
 	},
 	{
 		.appmodes = (const AppModeType **) &Task_C_AppModes,
 		.n_appmodes = TASK_C_APPMODE_MAX,
 		.evtmsks = (const EventMaskType**) &Task_C_EventMasks,
 		.n_evtmsks = TASK_C_EVENT_MAX,
-		.msglist = NULL
+		.msglist = NULL,
+		.n_msglist = TASK_C_MESSAGE_MAX
 	}
 };
