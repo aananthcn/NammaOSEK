da2b4986acf078d381e32d4fdb88d0d6a7987582
Aananth C N <c.n.aananth@gmail.com>
2021-08-19 07:03:21 +0530

    Added initial constructions for sg_alarms.c
    System Generator now start building the Alarms C source file based on
    configurtions made in OSEK-builder.xlsx or the OIL file output.

--- tools/scripts/sg_alarms.py
+++ tools/scripts/sg_alarms.py
@@ -1,5 +1,5 @@
 from common import print_info
-from ob_globals import TaskParams
+from ob_globals import AlarmParams, ANME, AAAT
 
 import colorama
 from colorama import Fore, Back, Style
@@ -12,9 +12,17 @@ C_AlarmAction_Type = "\n\ntypedef enum {\n\
     AAT_MAX_TYPE\n\
 } AlarmActionType;\n"
 
+
+AAT_PyList = {
+    "ACTIVATETASK" : "AAT_ACTIVATETASK",
+    "SETEVENT" : "AAT_SETEVENT",
+    "ALARMCALLBACK" : "AAT_ALARMCALLBACK"
+}
+
+
 C_Alarm_Type = "\n\ntypedef struct {\n\
     char* name;                     /* short name of alarm */ \n\
-    AlarmType counter_id;           /* OS Counter ID (= index of OsCounters + 1) */ \n\
+    AlarmType cntr_id;              /* OS Counter ID (= index of OsCounters + 1) */ \n\
     AlarmActionType aat;            /* Refer enum AlarmActionType */ \n\
     void* aat_arg1;                 /* arg1: task_name | callback_fun */\n\
     void* aat_arg2;                 /* arg2: event | NULL */\n\
@@ -39,6 +47,26 @@ def generate_code(path, Alarms):
     hf.write(C_Alarm_Type)
     hf.write("extern AppAlarmType AppAlarms[];\n")
 
+    # create source file
+    filename = path + "/" + "sg_alarms.c"
+    cf = open(filename, "w")
+    cf.write("#include <stddef.h>\n")
+    cf.write("#include <stdbool.h>\n")
+    cf.write("#include \"sg_alarms.h\"\n")
+    cf.write("\n\n/*   A L A R M S   D E F I N I T I O N S   */\n")
+    cf.write("const AppAlarmType AppAlarms[] = {\n")
+    for i, alarm in enumerate(Alarms):
+        cf.write("\t{\n")
+        cf.write("\t\t.name = "+alarm[AlarmParams[ANME]]+",\n")
+        cf.write("\t\t.cntr_id = "+str(i)+",\n")
+        cf.write("\t\t.aat = "+AAT_PyList[alarm[AlarmParams[AAAT]]]+",\n")
+
+        cf.write("\t}")
+        if i+1 < len(Alarms):
+            cf.write(",\n")
+        else:
+            cf.write("\n")
+    cf.write("};\n")
     print(Alarms)
     print("Alarms generate_code, is under construction\n")
     hf.write("\n\n#endif\n")
