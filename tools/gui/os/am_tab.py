641c31522f545165dcaf53af1b6928dcc880cf00
Aananth C N <c.n.aananth@gmail.com>
2022-01-06 23:52:14 +0530

    GUI: AppModes tab now displays modes correctly.

--- tools/gui/os/am_tab.py
+++ tools/gui/os/am_tab.py
@@ -2,10 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 
 N_AppModes = 1
+AppModes = None
 MaxAppModes = 16
 HeaderSize = 1
 HeaderObjs = 2 #Objects / widgets that are part of the header and shouldn't be destroyed
 
+AM_StrVar = []
 
 def n_appmodes(tab, modes):
     global N_AppModes
@@ -17,24 +19,43 @@ def n_appmodes(tab, modes):
     
 
 def draw_app_mode_tab(tab, appmodes):
-    global N_AppModes, AppModeObjs
-    N_AppModes_str = tk.StringVar(value=N_AppModes)
+    global N_AppModes, AppModeObjs, AM_StrVar, AppModes
+
+    AppModes = appmodes
+    N_AppModes = len(appmodes)
+    n_strvar = N_AppModes + 1
+    for i in range(n_strvar):
+        AM_StrVar.insert(i, tk.StringVar())
     
     #Number of modes - Label + Spinbox
     label = tk.Label(tab, text="Number of Modes ")
     label.grid(row=1, column=1, sticky="w")
-    spinb = tk.Spinbox(tab, width=28, values=tuple(range(1,MaxAppModes+1)),
-                       command=lambda: n_appmodes(tab, N_AppModes_str), textvariable=N_AppModes_str)
+    spinb = tk.Spinbox(tab, width=28, values=tuple(range(1,MaxAppModes+1)), textvariable=AM_StrVar[0], 
+                       command=lambda: n_appmodes(tab, AM_StrVar[0]))
+    AM_StrVar[0].set(N_AppModes)
     spinb.grid(row=1, column=2)
     update_app_mode_tab(tab)    
 
         
 def update_app_mode_tab(tab):
-    global N_AppModes, AppModeObjs
-    
+    global N_AppModes, AppModeObjs, AM_StrVar, AppModes
+
+    # StrVar memory allocation checks
+    n_am_strvar = len(AM_StrVar)-1
+    print("N_AppModes = "+str(N_AppModes)+", n_am_strvar = "+str(n_am_strvar))
+    if N_AppModes > n_am_strvar:
+        for i in range(N_AppModes - n_am_strvar):
+            AM_StrVar.insert(len(AM_StrVar), tk.StringVar())
+            AppModes.insert(len(AppModes), "")
+    elif n_am_strvar > N_AppModes:
+        for i in range(n_am_strvar - N_AppModes):
+            del AM_StrVar[-1]
+            del AppModes[-1]
+
     # Draw new objects
     for i in range(1, N_AppModes+1):
         label = tk.Label(tab, text="Mode "+str(i)+": ")
         label.grid(row=HeaderSize+i, column=1, sticky="w")
-        entry = tk.Entry(tab, width=30)
+        entry = tk.Entry(tab, width=30, textvariable=AM_StrVar[i])
+        AM_StrVar[i].set(AppModes[i-1])
         entry.grid(row=HeaderSize+i, column=2)
--- tools/gui/os/os_tab.py
+++ tools/gui/os/os_tab.py
@@ -1,132 +1,132 @@
 import tkinter as tk
 from tkinter import ttk
 
-N_Entries = 15
-OS_Entries = []
+N_StrVar = 15
+OS_StrVar = []
 
 def draw_os_tab(tab, oscfg):
-    for i in range(N_Entries):
-        OS_Entries.insert(i, tk.StringVar())
+    for i in range(N_StrVar):
+        OS_StrVar.insert(i, tk.StringVar())
         
     # 1) CPU / SoC - Label + Edit-box
     label = tk.Label(tab, text="CPU / SoC name")
     label.grid(row=1, column=1, sticky="w")
-    textb = tk.Entry(tab,text="Entry", width=30, textvariable=OS_Entries[0])
-    OS_Entries[0].set(oscfg["CPU"])
+    textb = tk.Entry(tab,text="Entry", width=30, textvariable=OS_StrVar[0])
+    OS_StrVar[0].set(oscfg["CPU"])
     textb.grid(row=1, column=2)
  
     # 2) OS Name - Label + Edit-box
     label = tk.Label(tab, text="Image Name")
     label.grid(row=2, column=1, sticky="w")
-    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_Entries[1])
-    OS_Entries[1].set(oscfg["OS"])
+    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_StrVar[1])
+    OS_StrVar[1].set(oscfg["OS"])
     textb.grid(row=2, column=2)
  
     # 3) OSEK Standard - Label + Combo-box
     label = tk.Label(tab, text="OSEK Standard")
     label.grid(row=3, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[2])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[2])
     osekstd['values'] = ("STANDARD", "EXTENDED")
-    OS_Entries[2].set(oscfg["STATUS"])
+    OS_StrVar[2].set(oscfg["STATUS"])
     osekstd.current()
     osekstd.grid(row=3, column=2)
 
     # 4) STARTUPHOOK - Label + Combo-box
     label = tk.Label(tab, text="STARTUPHOOK")
     label.grid(row=4, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[3])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[3])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[3].set(oscfg["STARTUPHOOK"])
+    OS_StrVar[3].set(oscfg["STARTUPHOOK"])
     osekstd.current()
     osekstd.grid(row=4, column=2)
  
     # 5) ERRORHOOK - Label + Combo-box
     label = tk.Label(tab, text="ERRORHOOK")
     label.grid(row=5, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[4])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[4])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[4].set(oscfg["ERRORHOOK"])
+    OS_StrVar[4].set(oscfg["ERRORHOOK"])
     osekstd.current()
     osekstd.grid(row=5, column=2)
 
     # 6) SHUTDOWNHOOK - Label + Combo-box
     label = tk.Label(tab, text="SHUTDOWNHOOK")
     label.grid(row=6, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[5])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[5])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[5].set(oscfg["SHUTDOWNHOOK"])
+    OS_StrVar[5].set(oscfg["SHUTDOWNHOOK"])
     osekstd.current()
     osekstd.grid(row=6, column=2)
 
     # 7) PRETASKHOOK - Label + Combo-box
     label = tk.Label(tab, text="PRETASKHOOK")
     label.grid(row=7, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[6])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[6])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[6].set(oscfg["PRETASKHOOK"])
+    OS_StrVar[6].set(oscfg["PRETASKHOOK"])
     osekstd.current()
     osekstd.grid(row=7, column=2)
 
     # 8) POSTTASKHOOK - Label + Combo-box
     label = tk.Label(tab, text="POSTTASKHOOK")
     label.grid(row=8, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[7])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[7])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[7].set(oscfg["POSTTASKHOOK"])
+    OS_StrVar[7].set(oscfg["POSTTASKHOOK"])
     osekstd.current()
     osekstd.grid(row=8, column=2)
 
     # 9) USEGETSERVICEID - Label + Combo-box
     label = tk.Label(tab, text="USEGETSERVICEID")
     label.grid(row=9, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[8])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[8])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[8].set(oscfg["USEGETSERVICEID"])
+    OS_StrVar[8].set(oscfg["USEGETSERVICEID"])
     osekstd.current()
     osekstd.grid(row=9, column=2)
 
     # 10) USEPARAMETERACCESS - Label + Combo-box
     label = tk.Label(tab, text="USEPARAMETERACCESS ")
     label.grid(row=10, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[9])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[9])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[9].set(oscfg["USEPARAMETERACCESS"])
+    OS_StrVar[9].set(oscfg["USEPARAMETERACCESS"])
     osekstd.current()
     osekstd.grid(row=10, column=2)
 
     # 11) USERESSCHEDULER - Label + Combo-box
     label = tk.Label(tab, text="USERESSCHEDULER")
     label.grid(row=11, column=1, sticky="w")
-    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_Entries[10])
+    osekstd = ttk.Combobox(tab, width=27, textvariable=OS_StrVar[10])
     osekstd['values'] = ("FALSE", "TRUE")
-    OS_Entries[10].set(oscfg["USERESSCHEDULER"])
+    OS_StrVar[10].set(oscfg["USERESSCHEDULER"])
     osekstd.current()
     osekstd.grid(row=11, column=2)
 
     # 12) OS_STACK_SIZE - Label + Edit-box
     label = tk.Label(tab, text="OS STACK SIZE")
     label.grid(row=12, column=1, sticky="w")
-    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_Entries[11])
-    OS_Entries[11].set(oscfg["OS_STACK_SIZE"])
+    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_StrVar[11])
+    OS_StrVar[11].set(oscfg["OS_STACK_SIZE"])
     textb.grid(row=12, column=2)
     
     # 13) OS_CTX_SAVE_SZ - Label + Edit-box
     label = tk.Label(tab, text="OS CONTEXT SAVE SIZE")
     label.grid(row=13, column=1, sticky="w")
-    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_Entries[12])
-    OS_Entries[12].set(oscfg["OS_CTX_SAVE_SZ"])
+    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_StrVar[12])
+    OS_StrVar[12].set(oscfg["OS_CTX_SAVE_SZ"])
     textb.grid(row=13, column=2)
 
     # 14) IRQ_STACK_SIZE - Label + Edit-box
     label = tk.Label(tab, text="IRQ STACK SIZE")
     label.grid(row=14, column=1, sticky="w")
-    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_Entries[13])
-    OS_Entries[13].set(oscfg["IRQ_STACK_SIZE"])
+    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_StrVar[13])
+    OS_StrVar[13].set(oscfg["IRQ_STACK_SIZE"])
     textb.grid(row=14, column=2)
     
     # 15) TASK_STACK_SIZE - Label + Edit-box
     label = tk.Label(tab, text="TASK STACK SIZE")
     label.grid(row=14, column=1, sticky="w")
-    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_Entries[14])
-    OS_Entries[14].set(oscfg["TASK_STACK_SIZE"])
+    textb = tk.Entry(tab, text="Entry", width=30, textvariable=OS_StrVar[14])
+    OS_StrVar[14].set(oscfg["TASK_STACK_SIZE"])
     textb.grid(row=14, column=2)
