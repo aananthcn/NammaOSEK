0cb99ea8a8c28e7422957f4446475fc5556aacb6
U-EMBITEL\emb-aanahcn <emb-aanahcn@EMB-BAN-921.embitel.local>
2022-06-19 23:06:29 +0530

    OSEK is not hitting the reset handler on Pico
    Till yesterday the pico-bootrom was never able to run the first line of
    code on OSEK os. It gets stuck or it takes exception. Now with these
    changes, these situation is improved. But I am seeing HardFaults, needs
    debugging...

--- board/rp2040/vectors.s
+++ board/rp2040/vectors.s
@@ -1,30 +1,63 @@
 .syntax unified
 .cpu cortex-m0plus
 .thumb
-.balign 2
+.align 2
  
 
-.text
+.section .vectors, "ax"
  /* .code 32 */
 
+.global _vectors_start
+.global _vectors_end
 _vectors_start:
-        .long	_os_stack_top
-        .long	(_Reset_handler+1)      /* Exception no  1, +1 is for setting the T-bit in EPSR */
-        .long	_NMI_handler 	        /* Exception no  2 */
-        .long	_HardFault_handler      /* Exception no  3 */
-        .long	0                       /* Reserved   -  4 */
-        .long	0                       /* Reserved   -  5 */
-        .long	0                       /* Reserved   -  6 */
-        .long	0                       /* Reserved   -  7 */
-        .long	0                       /* Reserved   -  8 */
-        .long	0                       /* Reserved   -  9 */
-        .long	0                       /* Reserved   - 10 */
-        .long	_SVCall_handler         /* Exception no 11 */
-        .long	0                       /* Reserved   - 12 */
-        .long	0                       /* Reserved   - 13 */
-        .long	_PendSV_handler         /* Exception no 14 */
-        .long	_SysTick_handler        /* Exception no 15 */
-        .long	_irq_handler            /* Exception no 16 */ 
+        .word	_os_stack_top
+        .word	(_Reset_handler)        /* Exception no  1 */
+        .word	_NMI_handler 	        /* Exception no  2 */
+        .word	_HardFault_handler      /* Exception no  3 */
+        .word	0                       /* Reserved   -  4 */
+        .word	0                       /* Reserved   -  5 */
+        .word	0                       /* Reserved   -  6 */
+        .word	0                       /* Reserved   -  7 */
+        .word	0                       /* Reserved   -  8 */
+        .word	0                       /* Reserved   -  9 */
+        .word	0                       /* Reserved   - 10 */
+        .word	_SVCall_handler         /* Exception no 11 */
+        .word	0                       /* Reserved   - 12 */
+        .word	0                       /* Reserved   - 13 */
+        .word	_PendSV_handler         /* Exception no 14 */
+        .word	_SysTick_handler        /* Exception no 15 */
+        .word	_irq_handler            /* Exception no 16 */ 
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
+        .word 	0
         
 _vectors_end:
         B .
