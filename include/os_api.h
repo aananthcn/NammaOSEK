88d9b4ebf64f31c90f0f68c54f63b0814e0dbd49
Aananth C N <c.n.aananth@gmail.com>
2024-01-10 12:27:43 +0530

    Disabled pr_log as printf is causing side effects.
    In task EcuM_StartupTwo(), the only function present is pr_log() but by
    the end of it, it clears _OsTaskDataBlk[4].activations to 0 :-). This
    will be analyzed later.

--- include/os_api.h
+++ include/os_api.h
@@ -15,23 +15,8 @@ u32 _GetOsTickCnt(void);
 int _OsHandleTicks(void);
 
 
-// /* low level APIs  - Stack Pointer Manipulation functions */
-// u32 _set_stack_ptr(u32 new_stack);
-// u32 _get_stack_ptr(void);
-// u32 _get_irq_stack_ptr(void);
-// u32 _get_next_pc(void);
-// void _set_sp_and_pc(u32 sp, u32 pc);
-// u32 _save_context(u32 sp);
-// void _restore_context(u32 sp);
-
-/* low level APIs  - Interrupt functions */
-// u32 _set_interrupt_bits(u32 if_bits);
-// u32 _clear_interrupt_bits(u32 if_bits);
-
-
-// int pr_log(const char *format, ...);
-// int pr_log_init(void);
-#define pr_log 	printf
+// logging functions
+#define pr_log(...) 	do {} while(0)
 
 
 #endif
